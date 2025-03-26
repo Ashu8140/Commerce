f3298d8cdb131e76482616d5d2722389016ecce6
Ashu <ashusainijijwania8140@gmail.com>
2025-03-26 08:41:06 +0530

    paragraph add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 export default function App() {
   return (
+    <div>
     <h1 className="text-3xl font-bold underline bg-red-500">
       Hello world!
     </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ad nobis molestias architecto! Amet ipsa esse atque minima, fugit perferendis porro nobis id perspiciatis? Suscipit itaque minima sequi veniam autem.</p>
+    </div>
   )
 }
